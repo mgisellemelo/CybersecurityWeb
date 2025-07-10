f77da58f5a2304ccc08fcabc302e5d93634f83fe
unknown <Giselle M>
2025-07-10 12:26:37 +0200

    modificaciones para render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,4 +1,5 @@
 from corsheaders.defaults import default_headers
+import os
 """
 Django settings for backend project.
 
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&upde50$p^ztp9dx44uyvl*7!cepw+6-avj^kd*%s4s&#d&l5h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -142,4 +143,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
 
