3fd40c97d113fd74ca479d9ddd1c8c5a984937e7
Ellesig <102600477+mgisellemelo@users.noreply.github.com>
2026-08-27 18:59:11 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,4 +1,5 @@
 from corsheaders.defaults import default_headers
+from decouple import config
 import os
 """
 Django settings for backend project.
@@ -24,7 +25,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&upde50$p^ztp9dx44uyvl*7!cepw+6-avj^kd*%s4s&#d&l5h'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
